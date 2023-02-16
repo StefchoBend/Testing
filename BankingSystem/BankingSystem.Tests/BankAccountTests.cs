b4d1250130fdea17cb6f6c50e165e9a61dc3b980
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-16 09:41:35 +0200

    add test class

---
+++ BankingSystem/BankingSystem.Tests/BankAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BankingSystem.Tests
+{
+    public class BankAccountTests
+    {
+        public void DepositShouldIncreaseBalance()
+        {
+            BankAccount bankAccount = new BankAccount(123);
+            decimal depositAmount = 100;
+            bankAccount.Deposit(depositAmount);
+            Assert.AreEqual(depositAmount, bankAccount.Balance);
+        }
+        public void AccountInicializeWithPositiveValue()
+        {
+            BankAccount bankAccount = new BankAccount(123, 2000m);
+            Assert.AreEqual(2000m, bankAccount.Balance);
+        }
+    }
+}
